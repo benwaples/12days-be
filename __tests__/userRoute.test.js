6debc17657de85af448cd21bb68cb7b92cc237c8
bwapes <benwaples@gmail.com>
2020-11-14 11:54:03 -0800

    validate logins

--- __tests__/userRoute.test.js
+++ __tests__/userRoute.test.js
@@ -62,4 +62,22 @@ describe('Auth Routes', () => {
   });
 
   //bad login credentials
+  it('should not log in a user with bad credentials via POST', async() => {
+    const user = {
+      username: 'benwa',
+      password: '1234',
+      avatarUrl: 'hi@url.com',
+      userRole: 'Director of Elf Technologies',
+    };
+
+    await request(app)
+      .post('/api/v1/auth/signup')
+      .send(user)
+      .then(res => expect(res.body).toEqual({ id: expect.any(String), username: 'benwa', avatarUrl: 'hi@url.com', userRole: 'Director of Elf Technologies' }));
+
+    return request(app)
+      .post('/api/v1/auth/login')
+      .send({ ...user, username: 'ben' })
+      .then(res => expect(res.body).toEqual({ message: 'Invalid username/password', status: 500 }));
+  });
 });
