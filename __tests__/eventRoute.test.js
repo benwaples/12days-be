e3123522d12906949dacbde6da5dd7a813b48ad3
bwapes <benwaples@gmail.com>
2020-11-14 13:17:23 -0800

    findById event

--- __tests__/eventRoute.test.js
+++ __tests__/eventRoute.test.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
+const Event = require('../lib/models/Event');
 
 describe('event route', () => {
   beforeEach(() => {
@@ -19,6 +20,21 @@ describe('event route', () => {
     return request(app)
       .post('/api/v1/events')
       .send(event)
+      .then(res => expect(res.body).toEqual({ ...event, id: expect.any(String) }));
+  });
+
+  it('should return an event by id via GET', async() => {
+    const event = {
+      name: 'walk',
+      description: 'walk around the park',
+      image: 'hi@test.com',
+      date: 'YYYY-MM-DD'
+    };
+
+    const insertedEvent = await Event.insert(event);
+
+    return request(app)
+      .get(`/api/v1/events/${insertedEvent.id}`)
       .then(res => expect(res.body).toEqual({ ...event, id: expect.any(String) }));
   });
 });
