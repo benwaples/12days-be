b369e04f64ee367bce42dbce5edda1df3c3f7d22
bwapes <benwaples@gmail.com>
2020-12-08 17:14:28 -0800

    test findByEventId and delete routes

--- __tests__/commentRoute.test.js
+++ __tests__/commentRoute.test.js
@@ -4,7 +4,13 @@ const request = require('supertest');
 const app = require('../lib/app');
 const Comment = require('../lib/models/comments/Comments');
 
-const comment = {
+const testComment = {
+  eventId: '1',
+  username: 'ben',
+  department: 'big pappa',
+  comment: 'wow nice project'
+};
+const testComment2 = {
   eventId: '1',
   username: 'ben',
   department: 'big pappa',
@@ -19,10 +25,25 @@ describe('comment route', () => {
   it('should insert a comment via POST', async() => {
     return request(app)
       .post('/api/v1/comments')
-      .send(comment)
-      .then(res => expect(res.body).toEqual({ ...comment, id: expect.any(String) }));
+      .send(testComment)
+      .then(res => expect(res.body).toEqual({ ...testComment, id: expect.any(String) }));
   });
 
-  // it('should return all comments via POST');
+  it('should return all comments for an event via GET', async() => {
+    const comment1 = await Comment.insert(testComment);
+    await Comment.insert(testComment2);
+
+    return request(app)
+      .get(`/api/v1/comments/${comment1.eventId}`)
+      .then(res => expect(res.body.length).toEqual(2));
+  });
+
+  it('should delete a comment given an id via DELETE', async() => {
+    const comment1 = await Comment.insert(testComment);
+
+    return request(app)
+      .delete(`/api/v1/comments/${comment1.eventId}`)
+      .then(res => expect(res.body).toEqual(comment1));
+  });
   
 });
